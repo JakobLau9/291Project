d68d39c9fd36e7bdcbfb81b348024f9a87e050a6
kooner27 <prabhnoo@ualberta.ca>
2022-11-22 15:19:53 -0700

    first part of venue

--- mp2/venue.py
+++ mp2/venue.py
@@ -7,5 +7,44 @@ from pprint import pprint
 def venueHandler(db, dblp, client):
     # For each venue, list the venue, the number of articles in that venue, and the number of articles that reference a paper in that venue. 
     # Sort the result based on the number of papers that reference the venue with the top most cited venues shown first. 
-    print("venue")
+    
+    #TODO: references part is not fast enough for the 1 million
+    # need to make a view or write some hard query
+    #TODO: maybe use projection with the group by so we don't need to for loop to print
+    # maybe store the venue:count in a dictionary if we're gonna do two separate queries
+    
+    # number of articles in that venue
+    y = db.dblp.aggregate([{"$group" : {"_id" : "$venue", "count":{"$sum":1}}}])
+    # yikes = db.dblp.aggregate([{"$group": {"_id":{"v":"$venue"}, "num": {"$sum":1}}}])
+    
+    
+    for item in y:
+        count = item["count"]
+        v = item["_id"]
+        print(f"{v} : {count}")
+    
+    
+    ###### NOT FAST ENOUGH FOR THE 1 MILLION
+    ##### NEED TO MAKE A VIEW OR WRTIE SOME HARD QUERY
+    
+    # print("venue")
+    # yikes = db.dblp.find({}, {"id":1, "venue":1, "_id":0})
+    
+    # ven_dict = {}
+    
+    # for x in yikes:
+    #     aid = x["id"]
+    #     venue = x["venue"]
+    #     results = db.dblp.find({"$text": {"$search": aid}})
+    #     for article in results:
+    #         # print(article)
+    #         # ref_article_id = article["id"]
+    #         # if (ref_article_id == aid):
+    #         if (venue in ven_dict):
+    #             ven_dict[venue] +=1
+    #         else:
+    #             ven_dict[venue] = 1
+
+    # print(ven_dict)
+            
     return
