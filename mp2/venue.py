4a72eec8c75cda909e0a3272fe52dc898b35a089
JakobLau9 <JakobLau9@gmail.com>
2022-11-23 20:56:00 -0700

    venue done?

--- mp2/venue.py
+++ mp2/venue.py
@@ -17,56 +17,113 @@ def venueHandler(db, dblp, client):
     #y = db.dblp.aggregate([{"$group" : {"_id" : "$venue", "count":{"$sum":1}}}])
     # yikes = db.dblp.aggregate([{"$group": {"_id":{"v":"$venue"}, "num": {"$sum":1}}}])
 
-    # n = int(input("How many venues would you like to see: "))
+    n = int(input("How many venues would you like to see: "))
 
-    # test = db.dblp.aggregate([
-    #     {'$match': {
-    #         'venue': {
-    #             '$ne':''
-    #         }
-    #     }},
-    #     { '$group': {
-    #         '_id': '$venue',
-    #         'articleCount': {'$sum': 1}
-    #     }
-    #     },
-    #     {'$sort': {'articleCount': -1}}
-    # ])
+    #test = db.dblp.aggregate([
+    #   {'$match': {
+    #        'venue': {
+    #            '$ne':''
+    #        }
+    #    }},
+    #    { '$group': {
+    #        '_id': '$venue',
+    #        'articleCount': {'$sum': 1}
+    #    }
+    #    },
+    #    {'$sort': {'articleCount': -1}}
+    #])
 
-    # count = 0
-    # for item in test:
-    #     count = count + 1
-    #     if (count > n):
-    #         break
-    #     print(item)
+    #count = 0
+    #for item in test:
+    #    count = count + 1
+    #    if (count > n):
+    #        break
+    #    print(item)
     
     #for item in y:
     #    count = item["count"]
     #    v = item["_id"]
     #    print(f"{v} : {count}")
-    
+ 
     
     ###### NOT FAST ENOUGH FOR THE 1 MILLION
     ##### NEED TO MAKE A VIEW OR WRTIE SOME HARD QUERY
     
-    print("venue")
-    yikes = db.dblp.find({}, {"id":1, "venue":1, "_id":0})
+    #print("venue")
+    #yikes = db.dblp.find({}, {"id":1, "venue":1, "_id":0})
     
-    ven_dict = {}
+    #en_dict = {}
     
-    for x in yikes:
-        aid = x["id"]
-        venue = x["venue"]
-        results = db.dblp.find({"$text": {"$search": aid}})
-        for article in results:
-            print(article)
-            ref_article_id = article["id"]
-            # if (ref_article_id == aid):
-            if (venue in ven_dict):
-                ven_dict[venue] +=1
-            else:
-                ven_dict[venue] = 1
+    #for x in yikes:
+    #    aid = x["id"]
+    #    venue = x["venue"]
+    #    results = db.dblp.find({"references": aid})
+    #    for article in results:
+    #        #print(article)
+    #        #ref_article_id = article["id"]
+    #        # if (ref_article_id == aid):
+    #        if (venue in ven_dict):
+    #            ven_dict[venue] +=1
+    #        else:
+    #            ven_dict[venue] = 1
 
-    print(ven_dict)
-            
+    #print("\n".join("{}\t{}".format(k, v) for k, v in ven_dict.items()))
+    
+    # key is the venue name and value is the list of articles in that venue
+    venue_articles = {}
+
+    # key is the article being referenced and value is the list of articles that reference it
+    references = {}
+
+    for x in db.dblp.find():
+        aid = x['id']
+
+        if 'venue' in x and x['venue'] != "":
+            vid = x['venue']
+            if vid in venue_articles:
+                venue_articles[vid].append(aid)
+            else:
+                venue_articles[vid] = [aid]
+        else:
+            continue
+
+        if aid not in references:
+            references[aid] = []
+
+        if 'references' in x:
+            referenceList = x['references']
+            for y in referenceList:
+                if y in references:
+                    references[y].append(aid)
+                else:
+                    references[y] = [aid]
+        else:
+            continue
+
+    
+    #print("\n".join("{}\t{}".format(k, v) for k, v in venue_articles.items()))
+
+
+
+    final_dict = {}
+    for key, value in venue_articles.items():
+        articleCount = 0
+        referenceCount = 0
+        for item in value:
+            articleCount += 1
+            referencesAmount = len(references[item])
+            referenceCount += referencesAmount
+        final_dict[key] = [articleCount, referenceCount]
+    
+    
+    sorted_dict = sorted(final_dict.items(), key = lambda item: item[1], reverse = True)
+
+    count = 0
+    for key, value in sorted_dict:
+        count += 1
+        if (count > n):
+            break
+        print("Venue: " + key + " Article Count: " + str(value[0]) + " References Count: " + str(value[1]))
+
+
     return
